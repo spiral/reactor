0e0099a351cd55afb3e5d4d2486e9a4bffb4eca0
Wolfy-J <wolfy.jd@gmail.com>
2017-02-07 16:03:38 +0300

    proper dependencies

--- source/Spiral/Reactor/DeclarationInterface.php
+++ source/Spiral/Reactor/DeclarationInterface.php
@@ -11,19 +11,6 @@ namespace Spiral\Reactor;
 /**
  * To be rendered with some indent.
  */
-interface DeclarationInterface
+interface DeclarationInterface extends \Spiral\Support\Serializer\DeclarationInterface
 {
-    /**
-     * Indent is always 4 spaces.
-     */
-    const INDENT = "    ";
-
-    /**
-     * Must render it's own content into string using given indent level.
-     *
-     * @param int $indentLevel
-     *
-     * @return string
-     */
-    public function render(int $indentLevel = 0): string;
 }
