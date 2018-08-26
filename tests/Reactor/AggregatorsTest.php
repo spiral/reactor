4334abe757ea7d692932fae48d8b11191a8f463d
Wolfy-J <wolfy.jd@gmail.com>
2018-08-26 11:18:33 +0300

    more tests

--- tests/Reactor/AggregatorsTest.php
+++ tests/Reactor/AggregatorsTest.php
@@ -9,8 +9,11 @@
 namespace Spiral\Tests\Reactor;
 
 use PHPUnit\Framework\TestCase;
+use Spiral\Reactor\Aggregator;
 use Spiral\Reactor\Aggregators\Methods;
 use Spiral\Reactor\Aggregators\Parameters;
+use Spiral\Reactor\Partials\Method;
+use Spiral\Reactor\Partials\Property;
 
 class AggregatorsTest extends TestCase
 {
@@ -30,5 +33,51 @@ class AggregatorsTest extends TestCase
         $this->assertNotNull($m);
 
         $this->assertSame($m, $aggr->get('param'));
+    }
+
+    /**
+     * @expectedException \Spiral\Reactor\Exceptions\ReactorException
+     */
+    public function testAggregator()
+    {
+        $a = new Aggregator([
+            Method::class
+        ]);
+
+        $a->add(new Property("method"));
+    }
+
+    /**
+     * @expectedException \Spiral\Reactor\Exceptions\ReactorException
+     */
+    public function testAggregatorNoElement()
+    {
+        $a = new Aggregator([
+            Method::class
+        ]);
+
+        $a->get("method");
+    }
+
+
+    public function testAggregatorRemove()
+    {
+        $a = new Aggregator([
+            Method::class
+        ]);
+
+        $a->add(new Method("method"));
+        $this->assertInstanceOf(Method::class, $a->method);
+        $this->assertTrue(isset($a['method']));
+        $this->assertInstanceOf(Method::class, $a['method']);
+
+        $this->assertTrue($a->has("method"));
+        $a->remove('method');
+        $this->assertFalse($a->has("method"));
+
+        $a['method'] = new Method('method');
+        $this->assertTrue($a->has("method"));
+        unset($a['method']);
+        $this->assertFalse($a->has("method"));
     }
 }
